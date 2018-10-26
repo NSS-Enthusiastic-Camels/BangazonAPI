6f918fbaa232a65f23208d466d4a82010b2dad82
unknown <alexfont321@gmail.com>
2018-10-26 15:16:40 -0500

    can see the customers

---
+++ BangazonApi/WebApplication1/Models/Customer.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+
+namespace BangazonAPI 
+{
+    public class Customer 
+    {
+    
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+
+    }
+    
+
+
+}
