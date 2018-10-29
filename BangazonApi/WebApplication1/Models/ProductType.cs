9befa87880059e37fced84943ce907e1045e87ae
priyankagargc27 <40770470+priyankagargc27@users.noreply.github.com>
2018-10-29 11:11:44 -0500

    Done with get,post,put and delete

---
+++ BangazonApi/WebApplication1/Models/ProductType.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace BangazonAPI.Data
+{
+    public class ProductType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+       
+    }
+
+}
