a5be7cba7e1fe4d727c6ae720fa5bf3ce1e1b4b5
Dejan Stjepanovic <ds2q88@gmail.com>
2018-10-30 15:09:18 -0500

    not done yet. Department

---
+++ BangazonApi/WebApplication1/Controllers/DepartmentController.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+
+namespace BangazonApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public DepartmentController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public IDbConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        // GET api/students?q=Taco
+        [HttpGet]
+        public async Task<IActionResult> Get(string q)
+        {
+            string sql = @"
+            SELECT
+                d.Id,
+                d.Name,
+                d.Budget
+            FROM Department d
+            WHERE 1=1
+            ";
+
+           // Console.WriteLine(sql);
+
+            using (IDbConnection conn = Connection)
+            {
+
+                IEnumerable<Department> departments = await conn.QueryAsync<Department>(
+                    sql
+                );
+                return Ok(departments);
+            }
+        }
+
+        // GET api/students/5
+        [HttpGet("{id}", Name = "GetDepartment")]
+        public async Task<IActionResult> Get([FromRoute]int id)
+        {
+            string sql = $@"
+            SELECT
+                d.Id,
+                d.Name,
+                d.Budget
+            FROM Department d
+            WHERE d.Id = {id}
+            ";
+
+            using (IDbConnection conn = Connection)
+            {
+                IEnumerable<Department> departments = await conn.QueryAsync<Department>(
+                    sql
+                );
+                return Ok(departments.Single());
+            }
+        }
+
+        // Add new one
+        // POST api/students
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Department department)
+        {
+            string sql = $@"INSERT INTO Department
+            (Name, Budget)
+            VALUES
+            (
+                '{department.Name}'
+                ,'{department.Budget}'
+            );
+            SELECT SCOPE_IDENTITY();";
+
+            using (IDbConnection conn = Connection)
+            {
+                var newId = (await conn.QueryAsync<int>(sql)).Single();
+                department.Id = newId;
+                return CreatedAtRoute("GetDepartment", new { id = newId }, department);
+            }
+        }
+
+        //edit
+        // PUT api/students/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] Department department)
+        {
+            string sql = $@"
+            UPDATE Department
+            SET 
+                Name = '{department.Name}',
+                Budget = {department.Budget}
+            WHERE Id = {id}";
+
+            // Console.WriteLine(sql);
+            try
+            {
+                using (IDbConnection conn = Connection)
+                {
+                    int rowsAffected = await conn.ExecuteAsync(sql);
+                    if (rowsAffected > 0)
+                    {
+                        return new StatusCodeResult(StatusCodes.Status204NoContent);
+                    }
+                    throw new Exception("No rows affected");
+                }
+            }
+            catch (Exception)
+            {
+                if (!DepartmentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        private bool DepartmentExists(int id)
+        {
+            string sql = $"SELECT Id FROM Department WHERE Id = {id}";
+            using (IDbConnection conn = Connection)
+            {
+                return conn.Query<Department>(sql).Count() > 0;
+            }
+        }
+
+
+    }
+}
